0646e3928071608ef707d45aa7f2f7c4192737af
Ubuntu <ubuntu@ip-172-31-5-209.ap-southeast-2.compute.internal>
2019-03-15 05:54:48 +0000

    blah

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nvidia-docker run -d -p 8888:8888 -p 6006:6006 --shm-size 50G -e PASSWORD=RickAndMorty -v ${PWD}:/notebook -v ${PWD}/data/:/notebook/data sachinruk/pytorch_gpu
+nvidia-docker run -d -p 8888:8888 -p 6006:6006 --shm-size 50G -e PASSWORD=ChangeThisPassword123 -v ${PWD}:/notebook -v ${PWD}/data/:/notebook/data sachinruk/pytorch_gpu
